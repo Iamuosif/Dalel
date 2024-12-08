ddf3240b5ad18a57221829d6c641d0e4dfa0939e
YosifAzab <yosifazab8@gmail.com>
2024-12-08 03:13:29 +0200

    App Router

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,10 @@
+abstract class AppStrings {
+  static const String dalel = 'Dalel';
+  static const String next = 'Next';
+  static const String welcome = 'Welcome';
+  static const String signin = 'Sign In';
+  static const String signup = 'Sign Up';
+  static const String createacc = 'Create Account';
+  static const String haveacc = 'Already have an account';
+  static const String login = 'Login Now';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:dalel/core/utils/app_assets.dart';
-import 'package:dalel/core/utils/app_colors.dart';
+import 'package:dalel/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,22 +10,9 @@ class Dalel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Dalel'),
-          ),
-          body: Container(
-            color: AppColors.primaryColor,
-            child: Text(
-              'Dalel',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold),
-            ),
-          )),
+      routerConfig: router,
     );
   }
 }
